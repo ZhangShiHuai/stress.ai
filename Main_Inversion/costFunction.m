7f848306e1f7f0492d326c42d7217c89f9316835
Shihuai Zhang <shihuaizhang.xh1@gmail.com>
2021-10-08 20:06:41 +0200

    Update costFunction.m

--- Main_Inversion/costFunction.m
+++ Main_Inversion/costFunction.m
@@ -1,34 +1,21 @@
-function [J, grad] = costFunction(theta, X, y)
+function [J, grad] = costFunction(w, x, y)
 %COSTFUNCTION Compute cost and gradient for logistic regression
-%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
-%   parameter for logistic regression and the gradient of the cost
-%   w.r.t. to the parameters.
-
+%
+%
 % Initialize some useful values
-m = length(y); % number of training examples
+m = length(y); % number of fractures
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
 %
-% Note: grad should have the same dimensions as theta
-%
-%=====================Compute J====================
+%=====================Compute J (cost) ====================
 
-H = sigmoid(X*theta);
+H = sigmoid(x*w);
 T = y.*log(H) + (1 - y).*log(1 - H);
 J = -1/m*sum(T);
 
-% ====================Compute grad==================
+% ====================Compute grad (gradient)==================
 
 for i = 1 : m
-	grad = grad + (H(i) - y(i)) * X(i,:)';
+	grad = grad + (H(i) - y(i)) * x(i,:)';
 end
 
 grad = 1/m*grad;
