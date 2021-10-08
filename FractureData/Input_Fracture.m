d5e57177d5da65cedacc449041cb47dc2d0b73dd
Shihuai Zhang <shihuaizhang.xh1@gmail.com>
2021-10-08 18:46:59 +0200

    Add files via upload

---
+++ FractureData/Input_Fracture.m
@@ -0,0 +1,11 @@
+% input file with fracture information
+%--------------------------------------------------------------------------
+% input_file = 'KTB.dat';
+% input_file = 'NTS.dat';
+% input_file = 'CajonPass.dat';
+input_file = 'LongValley.dat';
+
+
+
+
+
