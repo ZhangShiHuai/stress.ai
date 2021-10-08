a9ffb84ec2e9d6e00ce308d5d0bc0e5297a70e43
Shihuai Zhang <shihuaizhang.xh1@gmail.com>
2021-10-08 18:47:33 +0200

    Update Input_Fracture.m

--- FractureData/Input_Fracture.m
+++ FractureData/Input_Fracture.m
@@ -1,9 +1,7 @@
 % input file with fracture information
 %--------------------------------------------------------------------------
-% input_file = 'KTB.dat';
-% input_file = 'NTS.dat';
-% input_file = 'CajonPass.dat';
-input_file = 'LongValley.dat';
+input_file = 'CajonPass.dat';
+
 
 
 
