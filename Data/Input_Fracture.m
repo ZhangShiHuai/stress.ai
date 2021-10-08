f91386eca779d6e4d232aaaf60412195755877a4
Shihuai Zhang <shihuaizhang.xh1@gmail.com>
2021-10-08 22:26:21 +0200

    Delete Input_Fracture.m

--- Data/Input_Fracture.m
+++
@@ -1,9 +0,0 @@
-% input file with fracture information
-%--------------------------------------------------------------------------
-input_file = '../CajonPass/CajonPass.dat';
-
-output_file = '../CajonPass/Inversion_Output';
-
-
-
-
